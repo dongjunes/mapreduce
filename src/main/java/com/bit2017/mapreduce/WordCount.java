af7af45407dbf7f50db0fcb911e9ced32cbe72f7
dongjunes <melfs363@gmail.com>
2017-03-17 15:40:56 +0900

    hi

--- src/main/java/com/bit2017/mapreduce/WordCount.java
+++ src/main/java/com/bit2017/mapreduce/WordCount.java
@@ -1,30 +1,91 @@
 package com.bit2017.mapreduce;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount {
 
 	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
 
+		private LongWritable sumWritable = new LongWritable();
+
+		@Override
+		protected void reduce(Text key, Iterable<LongWritable> values,
+				Reducer<Text, LongWritable, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			long sum = 0;
+			for (LongWritable value : values) {
+				sum += value.get();
+			}
+			sumWritable.set(sum);
+			context.write(key, sumWritable);
+		}
+
 	}
 
 	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
+		private static LongWritable one = new LongWritable(1);
+		private Text words = new Text();
+
+		@Override
+		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer token = new StringTokenizer(line, "\r\n\t,|()<> ''");
+			while (token.hasMoreTokens()) {
+				words.set(token.nextToken());
+				context.write(words, one);
+
+			}
+
+		}
+
 	}
 
 	public static void main(String[] args) throws Exception {
-		Configuration conf=new Configuration();
-		Job job=new Job(conf,"WordCount");
-		
-		//job init
-		//job.setJarByClass(WordCount.class);
-	
-		//실행
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "WordCount");
+
+		// job init
+		job.setJarByClass(WordCount.class);
+
+		// mapper 지정
+		job.setMapperClass(MyMapper.class);
+
+		// reducer 지정
+		job.setReducerClass(MyReducer.class);
+
+		// 출력 키 타입
+		job.setOutputKeyClass(Text.class);
+
+		// 출력 타입지정
+		job.setMapOutputValueClass(LongWritable.class);
+
+		// 입력 파일포멧 지정(생략가능
+		job.setInputFormatClass(TextInputFormat.class);
+
+		// 입력 파일포멧 지정(생략가능
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		//입력파일 이름지정
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+
+		//출력디렉토리 지정
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		// 실행
 		job.waitForCompletion(true);
 	}
 
