dd1e0511f25bef80173a4e15aca3377bc1e22597
dongjunes <melfs363@gmail.com>
2017-03-23 17:45:27 +0900

    .

--- src/main/java/com/bit2017/mapreduce/textsearch/SearchDocs.java
+++ src/main/java/com/bit2017/mapreduce/textsearch/SearchDocs.java
@@ -12,8 +12,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 
 public class SearchDocs {
 	public static class MyMapper extends Mapper<Text, Text, Text, LongWritable> {
@@ -24,12 +26,12 @@ public class SearchDocs {
 		protected void map(Text key, Text value, Mapper<Text, Text, Text, LongWritable>.Context context)
 				throws IOException, InterruptedException {
 			Configuration conf = context.getConfiguration();
-			CharSequence c = conf.get("data");
+			String data = conf.get("data");
 
 			String line = value.toString();
 			StringTokenizer token = new StringTokenizer(line, "\r\n\t,|()<>''.:");
 			while (token.hasMoreTokens()) {
-				if (line.contains(c)) {
+				if (token.nextToken() == data) {
 					words.set(token.nextToken());
 					context.write(words, one);
 				}
@@ -64,15 +66,34 @@ public class SearchDocs {
 		conf.set("data", args[2]);
 
 		Job job = new Job(conf, "SearchDocs");
+		// job init
 		job.setJarByClass(SearchDocs.class);
+
+		// mapper 지정
 		job.setMapperClass(MyMapper.class);
+
+		// reducer 지정
 		job.setReducerClass(MyReducer.class);
+
+		// 출력 키 타입
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setInputFormatClass(KeyValueTextInputFormat.class);
+
+		// 출력 타입지정
+		job.setMapOutputValueClass(LongWritable.class);
+
+		// 입력 파일포멧 지정(생략가능
+		job.setInputFormatClass(TextInputFormat.class);
+
+		// 출력 파일포멧 지정(생략가능
 		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// 입력파일 이름지정
 		FileInputFormat.addInputPath(job, new Path(args[0]));
+
+		// 출력디렉토리 지정
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// 실행
 		job.waitForCompletion(true);
 
 	}
