e2e00f79f081ee75fc54888d947707d9ab4f4f8b
dongjunes <melfs363@gmail.com>
2017-03-24 13:24:33 +0900

    ..

--- src/main/java/com/bit2017/mapreduce/index/InvertedIndex1.java
+++ src/main/java/com/bit2017/mapreduce/index/InvertedIndex1.java
@@ -20,91 +20,86 @@ public class InvertedIndex1 {
 
 	public static class MyMapper extends Mapper<Text, Text, Text, Text> {
 
-		private Text word = new Text();
 		private Set<String> words = new HashSet<String>();
+		private Text word = new Text();
 
 		@Override
 		protected void map(Text docId, Text contents, Mapper<Text, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
-
 			String line = contents.toString();
-			StringTokenizer token = new StringTokenizer(line, "\r\n\t,|()<> ''.:");
+			StringTokenizer tokenizer = new StringTokenizer( line, "\r\n\t,|()<> ''.:" );
 			words.clear();
-			while( token.hasMoreTokens() ) {
-				words.add( token.nextToken().toLowerCase() );
+			
+			while( tokenizer.hasMoreTokens() ) {
+				words.add( tokenizer.nextToken().toLowerCase() );
 			}
 
 			for( String w : words ) {
 				word.set( w );
 				context.write( word, docId );
-			}
-
+			}		
 		}
-
 	}
 
 	public static class MyReducer extends Reducer<Text, Text, Text, Text> {
-
+		
 		@Override
-		protected void reduce(Text word, Iterable<Text> docIds, Reducer<Text, Text, Text, Text>.Context context)
-				throws IOException, InterruptedException {
-			StringBuilder s = new StringBuilder();
+		protected void reduce(Text word, Iterable<Text> docIds,
+				Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+			StringBuilder sb = new StringBuilder();
 			boolean isFirst = true;
-			for (Text docId : docIds) {
-				if (isFirst != true) {
-					s.append(",");
+			for( Text docId : docIds ) {
+				if( isFirst == false ) {
+					sb.append( "," );
 				} else {
 					isFirst = false;
 				}
-				s.append(docId.toString());
+				
+				sb.append( docId.toString() );
 			}
-			context.write(word, new Text(s.toString()));
+			
+			context.write( word, new Text( sb.toString() ) );
 		}
-
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = new Job(conf, "WordCount");
-
-		// job init
-		job.setJarByClass(InvertedIndex1.class);
-
-		// mapper 지정
-		job.setMapperClass(MyMapper.class);
-
-		// reducer 지정
-		job.setReducerClass(MyReducer.class);
-
-		// 리듀스 개수 지정
-		job.setNumReduceTasks(10);
-
-		// map출력 키 타입
-		job.setMapOutputKeyClass(Text.class);
-
-		// map출력 타입지정
-		job.setMapOutputValueClass(Text.class);
-
-		// reduce출력 키 타입
-		job.setOutputKeyClass(Text.class);
-
-		// reduce출력 타입지정
-		job.setOutputValueClass(Text.class);
-
-		// 입력 파일포멧 지정(생략가능
-		job.setInputFormatClass(KeyValueTextInputFormat.class);
-
-		// 출력 파일포멧 지정(생략가능
-		job.setOutputFormatClass(TextOutputFormat.class);
-
-		// 입력파일 이름지정
+		Job job = new Job( conf, "Inverted Index 1" );
+		
+		//Job Instance 초기화 작업
+		job.setJarByClass( InvertedIndex1.class );
+		
+		//맵퍼 클래스 지정
+		job.setMapperClass( MyMapper.class );
+		//리듀서 클래스 지정
+		job.setReducerClass( MyReducer.class);
+		//리듀스 개수 지정
+		job.setNumReduceTasks( 10 );
+		
+		//map 출력 키 타입
+		job.setMapOutputKeyClass( Text.class );
+		//map 출력 밸류 타입
+		job.setMapOutputValueClass( Text.class );
+		
+		
+		//reduce 출력 키 타입
+		job.setOutputKeyClass( Text.class );
+		//reduce 출력 밸류 타입
+		job.setOutputValueClass( Text.class );
+		
+		//입력파일 포맷 지정(생략 가능)
+		job.setInputFormatClass( KeyValueTextInputFormat.class );
+		//출력파일 포맷 지정(생략 가능)
+		job.setOutputFormatClass( TextOutputFormat.class );
+		
+		//입력 파일 이름 지정
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-
-		// 출력디렉토리 지정
+		
+		//출력 디렉토리 지정
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-		// 실행
-		job.waitForCompletion(true);
+		
+		//실행
+		job.waitForCompletion( true );
 	}
 
 }
